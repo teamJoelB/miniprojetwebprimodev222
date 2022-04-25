c0e19e2bca2d3de5ebf018a59d6e6e660a926314
dylan <dylan@192.168.0.14>
2022-04-25 23:20:27 +0200

    PARTIE Conseiller

--- src/java/fr/solutec/servlet/ConseillerConnexionServlet.java
+++ src/java/fr/solutec/servlet/ConseillerConnexionServlet.java
@@ -79,11 +79,10 @@ public class ConseillerConnexionServlet extends HttpServlet {
         String mdp = request.getParameter("mdp");
 
         try {
-            Conseiller co = ConseillerDao.getByMailAndPassword(mail, mdp);
-            request.setAttribute("conseiller", co);
+            Conseiller co = ConseillerDao.getByMailAndPassword(mail, mdp);           
             if (co != null) {
                 HttpSession session = request.getSession();
-                session.setAttribute("conseillerConnected", co);
+                session.setAttribute("Conseiller", co);
                 request.getRequestDispatcher("WEB-INF/PageConseiller.jsp").forward(request, response);
             } else {
                 request.setAttribute("errorMsg", "Identifiant ou mot de passe incorrecte");
